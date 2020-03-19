2a7f8b6b140fc8af138c0df546e8d9b9d9929cad
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-03-19 08:16:05 +0530

    actual tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using GRMakie
+
+using AbstractPlotting, MakieGallery
+
 using Test
 
-@testset "GRMakie.jl" begin
-    # Write your own tests here.
-end
+include("saving.jl")
+# 
+# database = MakieGallery.load_database(["short_tests.jl"]);
+#
+# filter!(database) do example
+#     !("3d" ∈ example.tags) &&
+#     !("heatmap" in entry.tags) &&
+#     !("image" in entry.tags) &&
+#     !(lowercase(entry.title) ∈ (
+#         "arrows on hemisphere", "cobweb plot", "lots_of_heatmaps",
+#         "streamplot animation", "test heatmap + image overlap", "lots of heatmaps"
+#     ))
+# end
+#
---
+++ test/saving.jl
@@ -0,0 +1,30 @@
+database = MakieGallery.load_database(["short_tests.jl"]);
+
+filter!(database) do example
+    !("3d" ∈ example.tags) &&
+    !("heatmap" in entry.tags) &&
+    !("image" in entry.tags) &&
+    !(lowercase(entry.title) ∈ (
+        "arrows on hemisphere", "cobweb plot", "lots_of_heatmaps",
+        "streamplot animation", "test heatmap + image overlap", "lots of heatmaps"
+    ))
+end
+
+format_save_path = joinpath(@__DIR__, "test_formats")
+isdir(format_save_path) && rm(format_save_path, recursive = true)
+mkpath(format_save_path)
+savepath(uid, fmt) = joinpath(format_save_path, "$uid.$fmt")
+
+@testset "Saving formats" begin
+    for fmt in ("png", "jpeg", "tiff", "bmp", "svg", "pdf", "tex", "eps")
+        for example in database
+            @test try
+                save(savepath(example.unique_name, fmt), MakieGallery.eval_example(example))
+                true
+            catch e
+                @warn "Saving $(example.unique_name) in format `$fmt` failed!" exception=(e, Base.catch_backtrace())
+                false
+            end
+        end
+    end
+end
