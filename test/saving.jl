4ccc868a44a4bf92f214769311d9a1c06451705a
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-03-19 09:07:24 +0530

    really fix tests

--- test/saving.jl
+++ test/saving.jl
@@ -2,9 +2,9 @@ database = MakieGallery.load_database(["short_tests.jl"]);
 
 filter!(database) do example
     !("3d" ∈ example.tags) &&
-    !("heatmap" in entry.tags) &&
-    !("image" in entry.tags) &&
-    !(lowercase(entry.title) ∈ (
+    !("heatmap" in example.tags) &&
+    !("image" in example.tags) &&
+    !(lowercase(example.title) ∈ (
         "arrows on hemisphere", "cobweb plot", "lots_of_heatmaps",
         "streamplot animation", "test heatmap + image overlap", "lots of heatmaps"
     ))
